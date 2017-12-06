7d0fcadf2af3780747ef9da3c3efd31b62013680
Uri Shaked <uri@salsa4fun.co.il>
2017-12-06 18:46:19 +0100

    feat: switch models from `svg` to `obj`

--- web/t-rex.js
+++ web/t-rex.js
@@ -3,27 +3,24 @@ const trex = document.querySelector('#t-rex');
 const remote = document.querySelector('#remote');
 const wobbleAnimation = document.querySelector('#t-rex-wobble');
 
-const cactusKinds = [
-  {svgFile: 'assets/cactus-1.svg', height: 9.14},
-  {svgFile: 'assets/cactus-2.svg', height: 9.14},
-  {svgFile: 'assets/cactus-5.svg', height: 8.95},
-  {svgFile: 'assets/cactus-6.svg', height: 5.55},
-];
+const cactusKinds = 7;
 
 const distance = 30;
-const cactusCount = 16;
+const cactusCount = 12;
 
 function addCacti() {
   for (i = 0; i < cactusCount; i++) {
     let entity = document.createElement('a-entity');
     const angle = 360. / cactusCount * i;
-    const cactus = cactusKinds[Math.floor(cactusKinds.length * Math.random())];
-    entity.setAttribute('svgfile', Object.assign({}, cactus, {
-      color: 'black',
-    }));
+    const cactusId = Math.ceil(cactusKinds * Math.random());
+    entity.setAttribute('obj-model', {
+      obj: `assets/cactus-${cactusId}.obj`
+    });
+    entity.setAttribute('scale', { x: 10, y: 10, z:10 });
+    entity.setAttribute('material', { color: 'black' });
     entity.setAttribute('position', {
       x: distance * Math.cos(angle / 180 * Math.PI),
-      y: cactus.height / 2,
+      y: 0,
       z: -distance * Math.sin(angle / 180 * Math.PI),
     });
     entity.setAttribute('rotation', {
