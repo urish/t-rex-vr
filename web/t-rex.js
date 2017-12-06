b9ed96df05a4937d56612f03e3bf9c8e05c36e34
Uri Shaked <uri@salsa4fun.co.il>
2017-12-06 15:10:34 +0100

    feat: implement cylindrical environment

--- web/t-rex.js
+++ web/t-rex.js
@@ -7,20 +7,29 @@ const cactusKinds = [
   {svgFile: '../assets/cactus-6.svg', height: 5.55},
 ];
 
-function addCactus() {
-  let entity = document.createElement('a-entity');
-  const cactus = cactusKinds[Math.floor(cactusKinds.length * Math.random())];
-  entity.setAttribute('svgfile', Object.assign({}, cactus, {
-    color: 'black',
-  }));
-  entity.setAttribute('position', '30 2 -10');
-  const cactusAnimation = document.querySelector('#cactusAnimation');
-  const clone = document.importNode(cactusAnimation.content, true);
-  clone.firstElementChild.addEventListener('animationend', () => {
-    scene.removeChild(entity);
-  });
-  entity.appendChild(clone);
-  scene.appendChild(entity);
+const distance = 30;
+const cactusCount = 16;
+
+function addCacti() {
+  for (i = 0; i < cactusCount; i++) {
+    let entity = document.createElement('a-entity');
+    const angle = 360. / cactusCount * i;
+    const cactus = cactusKinds[Math.floor(cactusKinds.length * Math.random())];
+    entity.setAttribute('svgfile', Object.assign({}, cactus, {
+      color: 'black',
+    }));
+    entity.setAttribute('position', {
+      x: distance * Math.cos(angle / 180 * Math.PI),
+      y: cactus.height / 2,
+      z: -distance * Math.sin(angle / 180 * Math.PI),
+    });
+    entity.setAttribute('rotation', {
+      x: 0,
+      y: angle + 90,
+      z: 0,
+    });
+    scene.appendChild(entity);  
+  }
 }
 
-setInterval(addCactus, 1000);
+addCacti();
